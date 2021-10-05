494938b0f05278956036dfc6bbec0b79cce32d59
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-10-05 15:33:58 +0200

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -19,53 +19,53 @@ data "oci_core_vnic" "InstanceVnic" {
 
 data "oci_identity_compartments" "nw_compartments" {
   compartment_id = var.tenancy_ocid
-  name              = "${var.service}_network_compartment"
+  name              = "${local.service}_network_compartment"
   compartment_id_in_subtree = true
   state                     = "ACTIVE"
 }
 
 data "oci_identity_compartments" "appdev_compartments" {
   compartment_id = var.tenancy_ocid
-  name                      = "${var.service}_application_compartment"
+  name                      = "${local.service}_application_compartment"
   compartment_id_in_subtree = true
   state                     = "ACTIVE"
 }
 
 data "oci_core_vcns" "vcns" {
   compartment_id = local.nw_compartment_ocid
-  display_name              = "${var.service}_1_vcn"
+  display_name              = "${local.service}_1_vcn"
   state                     = "AVAILABLE"
 }
 
 #data "oci_core_subnets" "app_subnets" {
 #  compartment_id = local.nw_compartment_ocid
-#  display_name              = "${var.service}-0-app-subnet"
+#  display_name              = "${local.service}-0-app-subnet"
 #  state                     = "AVAILABLE"
 #}
 
 #data "oci_core_subnets" "web_subnets" {
 #  compartment_id = local.nw_compartment_ocid
-#  display_name              = "${var.service}-0-web-subnet"
+#  display_name              = "${local.service}-0-web-subnet"
 #  state                     = "AVAILABLE"
 #}
 
 data "oci_core_network_security_groups" "app_nsgs" {
   compartment_id = local.nw_compartment_ocid
-  display_name              = "${var.service}_1_security_group"
+  display_name              = "${local.service}_1_security_group"
 }
 
 data "oci_core_network_security_groups" "lbr_nsgs" {
   compartment_id = local.nw_compartment_ocid
-  display_name              = "${var.service}_1_security_group"
+  display_name              = "${local.service}_1_security_group"
 }
 
 data "oci_core_internet_gateways" "igws" {
   compartment_id = local.nw_compartment_ocid
-  display_name              = "${var.service}_1_internet_gateway"
+  display_name              = "${local.service}_1_internet_gateway"
 }
 
 data "oci_core_nat_gateways" "ngws" {
   compartment_id = local.nw_compartment_ocid
-  display_name              = "${var.service}_1_nat_gateway"
+  display_name              = "${local.service}_1_nat_gateway"
 }
 
