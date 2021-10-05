b373699192ab328414f8449098f89e32668ed4f3
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-10-05 15:27:20 +0200

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,6 @@
-
-
 resource "oci_core_security_list" "appsecuritylist" {
   compartment_id = local.nw_compartment_ocid
-  display_name   = "${var.service}_1_app_securitylist"
+  display_name   = "${local.service}_1_app_securitylist"
   vcn_id         = local.vcn_id
 
   egress_security_rules {
@@ -70,7 +68,7 @@ resource "oci_core_security_list" "appsecuritylist" {
 resource "oci_core_route_table" "poolroutetable" {
   compartment_id = local.nw_compartment_ocid
   vcn_id         = local.vcn_id
-  display_name   = "${var.service}_1_pool_routetable"
+  display_name   = "${local.service}_1_pool_routetable"
 
   route_rules {
     cidr_block        = "0.0.0.0/0"
@@ -81,7 +79,7 @@ resource "oci_core_route_table" "poolroutetable" {
 resource "oci_core_route_table" "lbroutetable" {
   compartment_id = local.nw_compartment_ocid
   vcn_id         = local.vcn_id
-  display_name   = "${var.service}_1_lb_routetable"
+  display_name   = "${local.service}_1_lb_routetable"
 
   route_rules {
     cidr_block        = "0.0.0.0/0"
@@ -93,7 +91,7 @@ resource "oci_core_subnet" "poolnet" {
   cidr_block       = cidrsubnet(local.vcn_cidr, 4, 5)
   compartment_id   = local.nw_compartment_ocid
   vcn_id           = local.vcn_id
-  display_name     = "${var.service}_1_pool_subnet"
+  display_name     = "${local.service}_1_pool_subnet"
   dns_label        = "poolnet"
   security_list_ids = [oci_core_security_list.appsecuritylist.id]
   route_table_id      = oci_core_route_table.poolroutetable.id
@@ -104,7 +102,7 @@ resource "oci_core_subnet" "lbnet" {
   cidr_block       = cidrsubnet(local.vcn_cidr, 4, 6)
   compartment_id   = local.nw_compartment_ocid
   vcn_id           = local.vcn_id
-  display_name     = "${var.service}_1_lb_subnet"
+  display_name     = "${local.service}_1_lb_subnet"
   dns_label        = "lbnet"
   security_list_ids = [oci_core_security_list.appsecuritylist.id]
   route_table_id      = oci_core_route_table.lbroutetable.id
