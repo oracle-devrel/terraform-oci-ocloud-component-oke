a895f0f4083c72ce7d01b912147b19125102f3e0
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-10-05 13:22:59 +0200

    Update autoscalingConfiguration.tf

--- autoscalingConfiguration.tf
+++ autoscalingConfiguration.tf
@@ -14,7 +14,7 @@ resource "oci_autoscaling_auto_scaling_configuration" "auto_scaling_configuratio
             max = "6"
             min = "2"
         }
-        display_name = "${var.service}-0-app-autoscaling-policy"
+        display_name = "${var.service}_1_app_autoscaling_policy"
         is_enabled = "true"
         rules {
             action {
@@ -47,6 +47,6 @@ resource "oci_autoscaling_auto_scaling_configuration" "auto_scaling_configuratio
 
     }
     cool_down_in_seconds = "300"
-    display_name = "${var.service}-0-app-autoscaling-configuration"   
+    display_name = "${var.service}_1_app_autoscaling_configuration"   
     is_enabled = "true"
 }
