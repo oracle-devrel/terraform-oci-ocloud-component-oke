b95f7bed108d68d55d187d4b7325221a8be19be8
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-10-19 22:06:50 +0200

    Update okeServiceDeployment.tf

--- okeServiceDeployment.tf
+++ okeServiceDeployment.tf
@@ -1,20 +1,20 @@
 # https://docs.cloud.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengdownloadkubeconfigfile.htm#notes
 
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "1.13"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    kubernetes = {
+#      source  = "hashicorp/kubernetes"
+#      version = "1.13"
+#    }
+#  }
+#}
 
 
 
 # define the Kubernetes provider. Get the Kubernetes configuration and extract the cluster certificate, extract the commands and arguments to create an ExecCredential and execute this command
 provider "kubernetes" {
-  load_config_file       = "false"        # Workaround for tf k8s provider < 1.11.1 to work with ORM
-  # config_path            = "~/.kube/config"
+  # load_config_file       = "false"        # Workaround for tf k8s provider < 1.11.1 to work with ORM
+  config_path            = "~/.kube/config"
   cluster_ca_certificate = base64decode(yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["certificate-authority-data"])
   host                   = yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["server"]
   exec {
