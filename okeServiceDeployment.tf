9e17ff62d859a59cd6723fb4f6af4d521f90c347
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-10-19 21:48:15 +0200

    Update okeServiceDeployment.tf

--- okeServiceDeployment.tf
+++ okeServiceDeployment.tf
@@ -12,7 +12,7 @@
 
 # define the Kubernetes provider. Get the Kubernetes configuration and extract the cluster certificate, extract the commands and arguments to create an ExecCredential and execute this command
 provider "kubernetes" {
-  version                = "2.4.1"
+  version                = "1.13.4"
   load_config_file       = "false"        # Workaround for tf k8s provider < 1.11.1 to work with ORM
   cluster_ca_certificate = base64decode(yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["certificate-authority-data"])
   host                   = yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["server"]
