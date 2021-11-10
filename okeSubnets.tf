1843ddda455f1f94303a2c9a82b83a98647f816f
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-11-10 16:23:39 +0100

    Update okeSubnets.tf

--- okeSubnets.tf
+++ okeSubnets.tf
@@ -1,4 +1,4 @@
-variable "ingress_ports_nodepool_subnet_from_k8slb_subnet" {
+variable "ports_between_nodepool_subnet_and_k8slb_subnet" {
   type    = list(string)
-  default = ["30198", "10256", "31093", "32551", "30517", "80", "30943", "32206", "31866"]
+  default = ["30198", "10256", "31093", "32551", "30517", "80", "443", "30943", "32206", "31866"]
 }
