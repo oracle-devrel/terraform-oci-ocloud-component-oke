5602a9cce1923ac4b9213eaf33940a18fddfc673
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-11-10 17:59:34 +0100

    Update okeSubnets.tf

--- okeSubnets.tf
+++ okeSubnets.tf
@@ -10,7 +10,7 @@ resource "oci_core_subnet" "k8s" {
   vcn_id           = local.vcn_id
   display_name     = "${local.service}_k8s"
   dns_label        = "k8s"
-  security_list_ids = [oci_core_security_list.okenet_security_list.id]
+  security_list_ids = [oci_core_security_list.k8s_security_list.id]
   route_table_id      = local.pub_subnet_rt_id
   prohibit_public_ip_on_vnic = false
 }
@@ -21,7 +21,7 @@ resource "oci_core_subnet" "k8slb" {
   vcn_id           = local.vcn_id
   display_name     = "${local.service}_k8slb"
   dns_label        = "k8slb"
-  security_list_ids = [oci_core_security_list.oke_lb_security_list.id]
+  security_list_ids = [oci_core_security_list.k8slb_security_list.id]
   route_table_id      = local.pub_subnet_rt_id
   prohibit_public_ip_on_vnic = false
 }
@@ -32,29 +32,19 @@ resource "oci_core_subnet" "k8snodes" {
   vcn_id           = local.vcn_id
   display_name     = "${local.service}_k8snodes"
   dns_label        = "k8snodes"
-  security_list_ids = [oci_core_security_list.okenet_security_list.id]
+  security_list_ids = [oci_core_security_list.k8snodes_security_list.id]
   route_table_id      = local.priv_subnet_rt_id
   prohibit_public_ip_on_vnic = true
 }
 
-resource "oci_core_security_list" "okenet_security_list" {
+resource "oci_core_security_list" "k8s_security_list" {
   compartment_id = local.nw_compartment_ocid
-  display_name   = "${local.service}_1_oke_securitylist"
+  display_name   = "${local.service}_k8s_security_list"
   vcn_id         = local.vcn_id
 
   egress_security_rules {
     protocol    = "all"
     destination = "0.0.0.0/0"
-  }
-
-  ingress_security_rules {
-    tcp_options {
-      max = 22
-      min = 22
-    }
-    description = "External SSH access"
-    protocol = "6"
-    source   = "0.0.0.0/0"
   }
 
   ingress_security_rules {
@@ -66,6 +56,16 @@ resource "oci_core_security_list" "okenet_security_list" {
     protocol = "6"
     source   = "0.0.0.0/0"
   }
+  
+  ingress_security_rules {
+    tcp_options {
+      max = 6443
+      min = 6443
+    }
+    description = "Kubernetes worker to Kubernetes API endpoint communication"
+    protocol = "6"
+    source   = oci_core_subnet.k8snodes.cidr_block
+  }
 
   ingress_security_rules {
     tcp_options {
@@ -74,49 +74,23 @@ resource "oci_core_security_list" "okenet_security_list" {
     }
     description = "Kubernetes worker to control plane communication"
     protocol = "6"
-    source   = "0.0.0.0/0"
+    source   = oci_core_subnet.k8snodes.cidr_block
   }
   
-  ingress_security_rules {
-    source      = cidrsubnet(local.vcn_cidr, 4, 3)
-    protocol   = "all"
-    description = "Allow pods on one worker node to communicate with pods on other worker nodes and the K8S controller."
-  }
 
-  ingress_security_rules {
-    source      = cidrsubnet(local.vcn_cidr, 4, 4)
-    protocol   = "all"
-    description = "Allow inbound communication from load balancer subnet."
-  }
-  
-  ingress_security_rules {
-    icmp_options {
-      type = 0
-    }
-
-    protocol = 1
-    source   = "0.0.0.0/0"
-  }
   ingress_security_rules {
     icmp_options {
       type = 3
       code = 4
     }
-
+    description = "Path discovery"
     protocol = 1
-    source   = "0.0.0.0/0"
+    source   = oci_core_subnet.k8snodes.cidr_block
   }
-  ingress_security_rules {
-    icmp_options {
-      type = 8
-    }
 
-    protocol = 1
-    source   = "0.0.0.0/0"
-  }
 }
 
-resource "oci_core_security_list" "oke_lb_security_list" {
+resource "oci_core_security_list" "k8slb_security_list" {
   compartment_id = local.nw_compartment_ocid
   display_name   = "${local.service}_k8slb_security_list"
   vcn_id         = local.vcn_id
