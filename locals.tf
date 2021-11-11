02f4f3c6816f317b9181f866f3929dff428d1437
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-11-11 16:18:44 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -11,7 +11,7 @@ locals {
   nw_compartment_ocid = var.nw_compartment_id
   appdev_compartment_ocid = var.appdev_compartment_id
   subnet_id        = data.oci_core_subnets.app_subnets.subnets[0].id
-  nsg_id           = data.oci_core_network_security_groups.app_nsgs.network_security_groups[0].id 
+  #nsg_id           = data.oci_core_network_security_groups.app_nsgs.network_security_groups[0].id 
   igw_id           = data.oci_core_internet_gateways.igws.gateways[0].id
   ngw_id           = data.oci_core_nat_gateways.ngws.nat_gateways[0].id
   web_subnet_id        = data.oci_core_subnets.pres_subnets.subnets[0].id
