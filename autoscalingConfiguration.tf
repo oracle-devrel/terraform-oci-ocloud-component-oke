d211929090b5d845ca008e3b4a66453a2336afbc
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-11-11 11:13:51 +0100

    Update autoscalingConfiguration.tf

--- autoscalingConfiguration.tf
+++ autoscalingConfiguration.tf
@@ -1,3 +1,6 @@
+# Copyright (c) 2021 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 # auto-scaling configuration including one policy for the instance pool
 # scales out when CPU load > 80%, scales in when CPU load < 30%
 
