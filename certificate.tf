5dd37e3533ecef2c79b60477e482a5d8a1f5a31b
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-11-11 11:16:09 +0100

    Update certificate.tf

--- certificate.tf
+++ certificate.tf
@@ -1,3 +1,6 @@
+# Copyright (c) 2021 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 # creates an TLS private key and self-signed certificate (with a validity of one year)
 # for the usage in the load balancer listener and its HTTPS configuration
 
@@ -13,12 +16,12 @@ resource "tls_self_signed_cert" "demo_certificate" {
 
   subject {
     common_name         = "${local.service}_app_demo_certificate"
-    organization        = "Demo"
-    organizational_unit = "FOR TESTING ONLY"
+    organization        = "Test Demo Org"
+    organizational_unit = "THIS CERTIFICATE IS FOR TESTING PURPOSES ONLY"
   }
 
-  #1 year validity
-  validity_period_hours = 24 * 365
+  #valid for 30 days
+  validity_period_hours = 30 * 24
 
   allowed_uses = [
     "digital_signature",
@@ -28,11 +31,8 @@ resource "tls_self_signed_cert" "demo_certificate" {
 }
 
 resource "oci_load_balancer_certificate" "test_certificate" {
-    #Required
     certificate_name = "${local.service}_1_test_ssl_certificate"
     load_balancer_id = oci_load_balancer_load_balancer.load_balancer.id
-
-    #Optional
     private_key = tls_private_key.demo_private_key.private_key_pem
     public_certificate = tls_self_signed_cert.demo_certificate.cert_pem
     lifecycle {
