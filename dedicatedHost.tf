676aeb78898009e93a5faa7f74f80f0fae632fa5
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-11-11 11:21:10 +0100

    Update dedicatedHost.tf

--- dedicatedHost.tf
+++ dedicatedHost.tf
@@ -1,11 +1,14 @@
-# creates a Dedicated VM Host (commented out due to lack of available service limits in our tenant)
+# Copyright (c) 2021 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+# creates a Dedicated VM Host (commented out due to lack of available service limits in common tenants)
 
 /*
 resource "oci_core_dedicated_vm_host" "test_dedicated_vm_host" {
     availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[0]["name"] 
     compartment_id = local.appdev_compartment_ocid
     dedicated_vm_host_shape = "DVH.Standard2.52"
-    display_name = "${local.service}_1_app_dedicated_vm_host"
+    display_name = "${local.service}_dedicated_vm_host"
 }
 */
 
