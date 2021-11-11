64288678336c0972314436ba5c205f68446551fb
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-11-11 17:14:04 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -4,21 +4,19 @@
 # copy the discovered resources from datasource.tf into local variables
 
 locals {
-  home_region      = "eu-frankfurt-1"
-  service          = lower("${var.organization}_${var.project}")
-  vcn_id           = var.vcn_id
-  vcn_cidr         = data.oci_core_vcn.vcn.cidr_blocks[0]
-  nw_compartment_ocid = var.nw_compartment_id
-  appdev_compartment_ocid = var.appdev_compartment_id
-  subnet_id        = data.oci_core_subnets.app_subnets.subnets[0].id
-  #nsg_id           = data.oci_core_network_security_groups.app_nsgs.network_security_groups[0].id 
-  igw_id           = data.oci_core_internet_gateways.igws.gateways[0].id
-  ngw_id           = data.oci_core_nat_gateways.ngws.nat_gateways[0].id
-  web_subnet_id        = data.oci_core_subnets.pres_subnets.subnets[0].id
-  pub_subnet_rt_id     = data.oci_core_route_tables.pub_route_tables.route_tables[0].id
-  priv_subnet_rt_id     = data.oci_core_route_tables.priv_route_tables.route_tables[0].id
-  #lbr_nsg_id           = data.oci_core_network_security_groups.lbr_nsgs.network_security_groups[0].id 
-  k8s_cidr          = lookup(data.terraform_remote_state.external_stack_remote_state.outputs.service_segment_subnets, "k8s", "This CIDR is not defined") 
-  k8snodes_cidr          = lookup(data.terraform_remote_state.external_stack_remote_state.outputs.service_segment_subnets, "k8snodes", "This CIDR is not defined") 
-  k8slb_cidr          = lookup(data.terraform_remote_state.external_stack_remote_state.outputs.service_segment_subnets, "k8slb", "This CIDR is not defined")  
+  home_region               = "eu-frankfurt-1"
+  service                   = lower("${var.organization}_${var.project}")
+  vcn_id                    = var.vcn_id
+  vcn_cidr                  = data.oci_core_vcn.vcn.cidr_blocks[0]
+  nw_compartment_ocid       = var.nw_compartment_id
+  appdev_compartment_ocid   = var.appdev_compartment_id
+  subnet_id                 = data.oci_core_subnets.app_subnets.subnets[0].id
+  igw_id                    = data.oci_core_internet_gateways.igws.gateways[0].id
+  ngw_id                    = data.oci_core_nat_gateways.ngws.nat_gateways[0].id
+  web_subnet_id             = data.oci_core_subnets.pres_subnets.subnets[0].id
+  pub_subnet_rt_id          = data.oci_core_route_tables.pub_route_tables.route_tables[0].id
+  priv_subnet_rt_id         = data.oci_core_route_tables.priv_route_tables.route_tables[0].id
+  k8s_cidr                  = lookup(data.terraform_remote_state.external_stack_remote_state.outputs.service_segment_subnets, "k8s", "This CIDR is not defined") 
+  k8snodes_cidr             = lookup(data.terraform_remote_state.external_stack_remote_state.outputs.service_segment_subnets, "k8snodes", "This CIDR is not defined") 
+  k8slb_cidr                = lookup(data.terraform_remote_state.external_stack_remote_state.outputs.service_segment_subnets, "k8slb", "This CIDR is not defined")  
 }
