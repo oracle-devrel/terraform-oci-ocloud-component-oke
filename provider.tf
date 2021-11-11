5311f1af55d87042a8f021e531008694037a54ed
ilaue2 <89583342+ilaue2@users.noreply.github.com>
2021-11-11 12:22:20 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
+# Copyright (c) 2021 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 provider "oci" {
   tenancy_ocid         = var.tenancy_ocid
   user_ocid            = var.user_ocid
